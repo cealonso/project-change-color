5520433e8371f1fda5b1b09bff13cf0da7d91217
Claudio Alonso <cealonso@gmail.com>
2024-05-27 20:43:24 -0300

    primer proyecto

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const changeColorFDC437=()=> {
+  document.getElementById("lorem").style.color = "#FDC437";
+}
+
+const changeColor0c6834=()=> {
+  document.getElementById("lorem").style.color = "#0c6834";
+}
+
+const changeColor4a9ccf=()=> {
+  document.getElementById("lorem").style.color = "#4a9ccf";
+}
+
+const changeColor661f45=()=>{
+  document.getElementById("lorem").style.color = "#661f45";
+}
+
+const changeColorF43E3D=()=> {
+  document.getElementById("lorem").style.color = "#F43E3D";
+}
+
+document.getElementById("colorFDC437").addEventListener("click", changeColorFDC437);
+document.getElementById("color0c6834").addEventListener("click", changeColor0c6834);
+document.getElementById("color4a9ccf").addEventListener("click", changeColor4a9ccf);
+document.getElementById("color661f45").addEventListener("click", changeColor661f45);
+document.getElementById("colorF43E3D").addEventListener("click", changeColorF43E3D);
+
+
---
+++ js/prueba.js
@@ -0,0 +1,28 @@
+function changeBackground() {
+  
+    const symbols = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    let color = "#";
+    // Math.floor(Math.random() * (max - min + 1)) + min; max=15, min=0
+    for(let i=0;i<6;i++){
+        Math.floor(Math.random() * (16));
+    }
+
+
+}
